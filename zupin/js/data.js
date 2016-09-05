2cc46d33e3cf19ef364f8203c1fe176832d2413d
Exhuasted <410536612@qq.com>
2016-09-05 15:54:53 +0800

    zuopin

---
+++ zupin/js/data.js
@@ -0,0 +1,92 @@
+//数据文件
+var listData=[
+	{
+		"time":2010,
+		"content1":"Our first project ever! Ah, we were so young...",
+		"content2":"But we learned a few tricks"
+	},
+	{
+		"time":2011,
+		"content1":"The first real job, with real money involved.",
+		"content2":"And the phone starts ringing"
+	},
+	{
+		"time":2012,
+		"content1":"over 217458 projects and 70k clients later... ;-)",
+		"content2":"Still passionate"
+	},
+	{
+		"time":2013,
+		"content1":"Working on several interesting projects",
+		"content2":"Top secret kinda stuff."
+	},
+];
+
+
+
+var worksData = [
+//	{
+//		"img":"img/img2.jpg",
+//		"text":"用html+css并且结合html5和css3实现的在线简历，设计参考了网上优秀的简历模板。",
+//		"link":"javascript:;"
+//	},
+	{
+		"img":"img/gerenye.jpg",
+		"text":"原生javascript实现的全屏切换效果，这个作品的亮点就是里面所有的动画都是用css3实现，通过js控制相应的class。",
+		"link":"http://x710894881.github.io/demos/designeat/"
+	},
+	{
+		"img":"img/tianmao.jpg",
+		"text":"这是模仿天猫主页写的一个静态页面，也算是自己平时布局的一个小练习吧。",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/xiangyingshi.jpg",
+		"text":"这是仿的UE首页的一个静态页面，用到了响应式布局也是对自己html+css的锻炼。",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/weiyun.jpg",
+		"text":"用原生js实现的腾讯微云网页版，实现了基本的新建，删除，重命名，全选，框选等主要功能。",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/hash.jpg",
+		"text":"这是一个主要运用BOM知识写的知识，通过改变地址栏信息hash值等，无刷新的获取数据，然后对数据进行渲染。",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/xiaoyouxi.jpg",
+		"text":"这是一个原生JS写的一个QQ表情小游戏，使用了Js的运动框架，鼠标点击表情震动消失，未点击碰到底部时屏幕会晃动，同时记录了失误和得分的数据。",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/yidongduan.jpg",
+		"text":"这是仿写天猫移动端布局，是一个静态页面。",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/rili.jpg",
+		"text":"这是一个原生JS写的一个日历的小demo实现了日历的基本功能",
+		"link":"javascript:;"
+	},
+	{
+		"img":"img/shizhong.jpg",
+		"text":"这是一个原生JS写的一个电子时钟效果，使用了JS的运动框架。",
+		"link":"javascript:;"
+	}
+];
+
+
+var imgArr = [
+		"img/img01.jpg",
+		"img/img02.jpg",
+		"img/img03.jpg",
+		"img/img04.png",
+		"img/img05.png",
+		"img/img06.png",
+		"img/img07.jpg",
+		"img/img08.png",
+		"img/img09.png",
+		"img/img01.png"
+]
