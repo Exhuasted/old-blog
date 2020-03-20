46e15625efbdf5cceee66a1a11db300abb57b52d
wobuzhidaomingzikeyiyouduochangshizhendebuzhidaoyouduochang <zhangxian@daling.com>
2020-03-20 23:28:37 +0800

    add

--- blog/docs/.vuepress/config.js
+++ blog/docs/.vuepress/config.js
@@ -2,6 +2,6 @@ module.exports = {
     title: "前端日志",
     description: "欢迎访问我的前端日志",
     head: [
-      ['link', { rel: 'icon', href: '/hero.jpeg' }]
+      ['link', { rel: 'icon', href: '/logo.jpeg' }]
     ]
   };
